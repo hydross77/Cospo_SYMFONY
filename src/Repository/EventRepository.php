1b1f4aa58cf2c97e24725d497a330bd40d383941
Boni77 <tiffany.dellmann@gmail.com>
2022-09-06 11:50:47 +0200

    comment

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -2,12 +2,13 @@
 
 namespace App\Repository;
 
+use DateTime;
+use App\Entity\User;
 use App\Entity\Event;
 use App\Entity\Level;
 use App\Entity\Sport;
-use App\Entity\User;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Event>
@@ -25,37 +26,44 @@ class EventRepository extends ServiceEntityRepository
     }
 
     /**
-     * Récupère les événement en lien avec une recherche.
-     *
+     * Récupère les événement en lien avec une recherche
      * @return Event[]
      */
-    public function findSearch(array $parameters)
+    public function findSearch(array $parameters) //parameters est le tableau
     {
         $qb = $this->createQueryBuilder('E');
+        // on prend le formulaire qu'on lie à 'E' : 'EVENT'
 
         $result = $qb->addSelect('E.title_event, E.nb_places, E.content_event, E.date_event, E.cp, E.ville, E.adresse')
+            // on recupere ce qu'on à besoin dans la base de données
             ->addSelect('U.pseudo')
+            // on selectionne le pseudo dans user pour le joindre :
             ->join(User::class, 'U', 'WITH', 'U.id = E.user')
+            // on le joint a l'id de la table User
             ->addSelect('S.title_sport')
             ->join(Sport::class, 'S', 'WITH', 'S.id = E.sport')
             ->addSelect('L.title_level')
             ->join(Level::class, 'L', 'WITH', 'L.id = E.level');
 
         if ($parameters['q'] !== null) {
+            // si le pseudo est différent de null une fois submit, on le cherche içi :
             $result = $qb->andWhere('U.pseudo = :pseudo')
                 ->setParameter('pseudo', $parameters['q']);
+            // lie 'q' du tableau $parameter au pseudo de la table user // bindParamPDO
         }
 
         if (!empty($parameters['sport']->toArray())) {
-            $sports = [];
-            $sp = $parameters['sport']->toArray();
-            foreach ($sp as $sport) {
-                array_push($sports, $sport->getId());
+            // si l'utiliser selectionne une ou des options, on le transforme en tableau ->toArray() des id selectionnés
+            $sports = []; // tableau vide
+            $sp = $parameters['sport']->toArray(); // on recupère le tableau des id selectionnés
+            foreach ($sp as $sport) { // on met les options selectionné dans le tableau vide de sport[]
+                array_push($sports, $sport->getId()); //empile une ou plusieurs options à la fin d'un tableau
             }
-            $result = $qb->andWhere('S.id IN (\'' . implode("','", $sports) . '\')');
+            $result = $qb->andWhere('S.id IN (\'' . implode("','", $sports) . '\')'); // resultat des ID implode en string
         }
 
         if (!empty($parameters['level']->toArray())) {
+            //pareil IF de sport
             $levels = [];
             $lv = $parameters['level']->toArray();
             foreach ($lv as $level) {
@@ -70,9 +78,9 @@ class EventRepository extends ServiceEntityRepository
         }
 
         $result = $qb->getQuery()
-            ->getResult();
+            ->getResult(); // requete bdd
 
-        return $result;
+        return $result; //retourne le tableau des résultats
     }
 
     public function add(Event $entity, bool $flush = false): void
@@ -98,8 +106,7 @@ class EventRepository extends ServiceEntityRepository
      */
     public function FuturEvent(): array
     {
-        $now = new \DateTime();
-
+        $now = new DateTime();
         return $this->createQueryBuilder('e')
             ->andWhere('e.date_event > :val')
             ->setParameter('val', $now)
@@ -113,8 +120,7 @@ class EventRepository extends ServiceEntityRepository
      */
     public function PastEvent(): array
     {
-        $now = new \DateTime();
-
+        $now = new DateTime();
         return $this->createQueryBuilder('e')
             ->andWhere('e.date_event < :val')
             ->setParameter('val', $now)
@@ -128,8 +134,7 @@ class EventRepository extends ServiceEntityRepository
      */
     public function PresentEvent(): array
     {
-        $now = new \DateTime();
-
+        $now = new DateTime();
         return $this->createQueryBuilder('e')
             ->Where('e.date_event = :val')
             ->setParameter('val', $now)
@@ -147,4 +152,6 @@ class EventRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+
+
 }
